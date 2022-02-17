7ffba7b4cfbbceeb7ac359398ab06153c15c5573
Ade Yansyah <Ade Yansyah@LAPTOP-7VR2D8CD>
2022-02-17 17:25:56 +0700

    import nonlb3

---
+++ WebApp/Repository/ImportNonLb3Model.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Repository
+{
+    public class ImportNonLb3Model
+    {
+        public int ehs_area_id { get; set; }
+        public int ba_id { get; set; }
+        public int pa_id { get; set; }
+        public int psa_id { get; set; }
+        public int bulan { get; set; }
+        public int tahun { get; set; }
+        public string jenis_limbah_id { get; set; }
+        public double timbulan_limbah_cair { get; set; }
+        public double timbulan_limbah_padat { get; set; }
+        public string deskripsi_limbah { get; set; }
+        public int pengelolaan_oleh_id { get; set; }
+        public int usaha_kurang_limbah_id { get; set; }
+        public string deskripsi_usaha { get; set; }
+        public string deskripsi_usaha_file_path { get; set; }
+        public double usaha_kurang_limbah_m3 { get; set; }
+        public double usaha_kurang_limbah_kg { get; set; }
+    }
+}
--- WebApp/Repository/Lb3Repository.cs
+++ WebApp/Repository/Lb3Repository.cs
@@ -98,5 +98,77 @@ namespace WebApp.Repository
             db.Close();
             return result;
         }
+        public int ImportNonLb3(ImportNonLb3Model importModel)
+        {
+            var lastId = getLastIdNonLb3();
+            var query = @"INSERT INTO ta_nonlb3
+                            ([id]
+                              ,[ehs_area_id]
+                              ,[ba_id]
+                              ,[pa_id]
+                              ,[psa_id]
+                              ,[bulan]
+                              ,[tahun]
+                              ,[jenis_limbah_id]
+                              ,[timbulan_limbah_cair]
+                              ,[timbulan_limbah_padat]
+                              ,[deskripsi_limbah]
+                              ,[pengelolaan_oleh_id]
+                              ,[usaha_kurang_limbah_id]
+                              ,[deskripsi_usaha]
+                              ,[deskripsi_usaha_file_path]
+                              ,[usaha_kurang_limbah_m3]
+                              ,[usaha_kurang_limbah_kg])
+                        Values
+                            (@id
+                              ,@ehs_area_id
+                              ,@ba_id
+                              ,@pa_id
+                              ,@psa_id
+                              ,@bulan
+                              ,@tahun
+                              ,@jenis_limbah_id
+                              ,@timbulan_limbah_cair
+                              ,@timbulan_limbah_padat
+                              ,@deskripsi_limbah
+                              ,@pengelolaan_oleh_id
+                              ,@usaha_kurang_limbah_id
+                              ,@deskripsi_usaha
+                              ,@deskripsi_usaha_file_path
+                              ,@usaha_kurang_limbah_m3
+                              ,@usaha_kurang_limbah_kg)";
+            var parameters = new
+            {
+                id = lastId + 1,
+                importModel.ehs_area_id,
+                importModel.ba_id,
+                importModel.pa_id,
+                importModel.psa_id,
+                importModel.bulan,
+                importModel.tahun,
+                importModel.jenis_limbah_id,
+                importModel.timbulan_limbah_cair,
+                importModel.timbulan_limbah_padat,
+                importModel.deskripsi_limbah,
+                importModel.pengelolaan_oleh_id,
+                importModel.usaha_kurang_limbah_id,
+                importModel.deskripsi_usaha,
+                importModel.deskripsi_usaha_file_path,
+                importModel.usaha_kurang_limbah_m3,
+                importModel.usaha_kurang_limbah_kg,
+            };
+            db.Open();
+            var result = db.Execute(query, parameters);
+            db.Close();
+            return result;
+        }
+        private int getLastIdNonLb3()
+        {
+            var query = "select MAX(id) as id from ta_nonlb3";
+            db.Open();
+            var result = db.ExecuteScalar<int>(query);
+            db.Close();
+            return result;
+        }
     }
 }
