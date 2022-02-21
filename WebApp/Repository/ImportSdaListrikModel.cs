91c13d9fac776e47a5aafb7f162b15b7f34e18e8
Ade Yansyah <Ade Yansyah@LAPTOP-7VR2D8CD>
2022-02-21 16:07:46 +0700

    Import sda Listrik

---
+++ WebApp/Repository/ImportSdaListrikModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Repository
+{
+    public class ImportSdaListrikModel
+    {
+        public int ehs_area_id { get; set; }
+        public int ba_id { get; set; }
+        public int pa_id { get; set; }
+        public int psa_id { get; set; }
+        public int bulan { get; set; }
+        public int tahun { get; set; }
+        public int sumber_listrik_id { get; set; }
+        public string no_rek_listrik { get; set; }
+        public double konsumsi_listrik { get; set; }
+        public string tagihan_listrik { get; set; }
+        public int usaha_pengurangan_listrik_id { get; set; }
+        public string usaha_pengurangan_listrik_desc { get; set; }
+        public string usaha_pengurangan_listrik_desc_file_path { get; set; }
+        public double usaha_pengurangan_listrik_jumlah { get; set; }
+    }
+}
