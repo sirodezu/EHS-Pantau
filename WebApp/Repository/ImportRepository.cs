15ee9a3bf390bee8a83964b74db24b6140105768
Ade Yansyah <Ade Yansyah@LAPTOP-7VR2D8CD>
2022-02-22 15:57:27 +0700

    perbaikan import non lb3

--- WebApp/Repository/ImportRepository.cs
+++ WebApp/Repository/ImportRepository.cs
@@ -117,37 +117,36 @@ namespace WebApp.Repository
                               ,[bulan]
                               ,[tahun]
                               ,[jenis_limbah_id]
-                              ,[timbulan_limbah_cair]
-                              ,[timbulan_limbah_padat]
+                              --,[timbulan_limbah_cair]
+                              --,[timbulan_limbah_padat]
                               ,[deskripsi_limbah]
                               ,[pengelolaan_oleh_id]
                               ,[usaha_kurang_limbah_id]
                               ,[deskripsi_usaha]
                               ,[deskripsi_usaha_file_path]
-                              ,[usaha_kurang_limbah_m3]
-                              ,[usaha_kurang_limbah_kg]
+                              --,[usaha_kurang_limbah_m3]
+                             -- ,[usaha_kurang_limbah_kg]
                               ,[insert_by]
                               ,[insert_at]
                               ,[update_by]
                               ,[update_at])
                         Values
-                            (@id
-                              ,@ehs_area_id
-                              ,@ba_id
-                              ,@pa_id
-                              ,@psa_id
-                              ,@bulan
-                              ,@tahun
-                              ,@jenis_limbah_id
-                              ,@timbulan_limbah_cair
-                              ,@timbulan_limbah_padat
+                            ((select max (id) + 1 from ta_nonlb3),(select id from ref_ehs_area where nama = @ehs_area_id)
+                              ,(select id from ref_business_area where nama = @ba_id)
+                              ,(select id from ref_personal_area where nama = @pa_id)
+                              ,(select id from ref_personal_sub_area where nama = @psa_id)
+                              ,(select id from ref_literal_data where cat_id = 150 and nama = @bulan)
+                              ,(select id from ref_literal_data where cat_id = 151 and nama = @tahun)
+                              ,(select id from ref_literal_data where cat_id = 98 and nama = @jenis_limbah_id)
+                              --,@timbulan_limbah_cair
+                              --,@timbulan_limbah_padat
                               ,@deskripsi_limbah
-                              ,@pengelolaan_oleh_id
-                              ,@usaha_kurang_limbah_id
+                              ,(select id from ref_literal_data where cat_id = 99 and nama = @pengelolaan_oleh_id)
+                              ,(select id from ref_literal_data where cat_id = 137 and nama = @usaha_kurang_limbah_id)
                               ,@deskripsi_usaha
                               ,@deskripsi_usaha_file_path
-                              ,@usaha_kurang_limbah_m3
-                              ,@usaha_kurang_limbah_kg
+                              --,@usaha_kurang_limbah_m3
+                              --,@usaha_kurang_limbah_kg
                               ,1
                               ,getdate()
                               ,1
@@ -162,15 +161,15 @@ namespace WebApp.Repository
                 importModel.bulan,
                 importModel.tahun,
                 importModel.jenis_limbah_id,
-                importModel.timbulan_limbah_cair,
-                importModel.timbulan_limbah_padat,
+                //importModel.timbulan_limbah_cair,
+                //importModel.timbulan_limbah_padat,
                 importModel.deskripsi_limbah,
                 importModel.pengelolaan_oleh_id,
                 importModel.usaha_kurang_limbah_id,
                 importModel.deskripsi_usaha,
                 importModel.deskripsi_usaha_file_path,
-                importModel.usaha_kurang_limbah_m3,
-                importModel.usaha_kurang_limbah_kg,
+                //importModel.usaha_kurang_limbah_m3,
+                //importModel.usaha_kurang_limbah_kg,
             };
             db.Open();
             var result = db.Execute(query, parameters);
